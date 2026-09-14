8adb3b588d2d05daedc942a9f29ad4c8b3cc8df9
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-09-15 00:19:57 +0530

    refactor: replace tabs with stack navigation in root layout
    - Remove Tab navigation implementation
    - Initialize Stack navigation with hidden headers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,8 @@
-import { Tabs } from "expo-router";
-import { usePathname } from "expo-router";
-import Animated, {FadeIn, FadeOut, SlideInRight, SlideOutLeft} from "react-native-reanimated";
+import { Stack } from "expo-router";
 
 
 export default function TabLayout() {
     return (
-        <Tabs screenOptions={{
-            animation: 'shift'
-        }}>
-            <Tabs.Screen name="index" options={{title: 'Home'}}/>
-            <Tabs.Screen name="schedule" options={{title: 'Schedule'}}/>
-
-        </Tabs>
+        <Stack screenOptions={{headerShown: false}}/>
     );
 }
